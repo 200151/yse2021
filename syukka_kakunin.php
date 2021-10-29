12d6dc1a145e4d04a67fdce15e3045bed7cd527f
AidaShuki <200001md@yse-c.net>
2021-10-29 10:00:52 +0900

    fit

--- syukka_kakunin.php
+++ syukka_kakunin.php
@@ -9,6 +9,7 @@
 */
 
 //①セッションを開始する
+session_start();
 
 function getByid($id,$con){
 	/* 
@@ -16,6 +17,12 @@ function getByid($id,$con){
 	 * その際にWHERE句でメソッドの引数の$idに一致する書籍のみ取得する。
 	 * SQLの実行結果を変数に保存する。
 	 */
+
+	$sql ="SELECT * FROM books WHERE :id = id";
+	$stmt = $con->prepare($sql);
+	$stmt->execute([":id" => $id]);
+
+	return $stmt->fetch();
 
 	//③実行した結果から1レコード取得し、returnで値を返す。
 }
@@ -26,64 +33,112 @@ function updateByid($id,$con,$total){
 	 * 引数で受け取った$totalの値で在庫数を上書く。
 	 * その際にWHERE句でメソッドの引数に$idに一致する書籍のみ取得する。
 	 */
+
+	$sql = "UPDATE books SET stock=:total WHERE :id = id";
+	$stmt = $con->prepare($sql);
+	// var_dump($sql);
+	// exit;
+	$stmt->execute([":total" => $total,":id" => $id]);
 }
 
 //⑤SESSIONの「login」フラグがfalseか判定する。「login」フラグがfalseの場合はif文の中に入る。
-if (/* ⑤の処理を書く */){
+if (empty($_SESSION['login'])){
 	//⑥SESSIONの「error2」に「ログインしてください」と設定する。
 	//⑦ログイン画面へ遷移する。
+	$_SESSION['error'] == 'ログインしてください。';
+	header('Location: login.php');
 }
 
 //⑧データベースへ接続し、接続情報を変数に保存する
+
+$db_name = "zaiko2021_yse";
+$db_host = "localhost";
+$db_port = "3306";
+$db_user = "zaiko2021_yse";
+$db_password = "2021zaiko";
+$dsn = "mysql:dbname={$db_name};host={$db_host};charset=utf8;port={$db_port}";
+
+try{
+	$pdo = new PDO($dsn,$db_user,$db_password);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+	$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+}catch(PDOException $e){
+	echo "接続エラー: ".$e->getMessage();
+	exit();
+
+}
 
 //⑨データベースで使用する文字コードを「UTF8」にする
 
 //⑩書籍数をカウントするための変数を宣言し、値を0で初期化する
 
+$book_count = 0;
+
 //⑪POSTの「books」から値を取得し、変数に設定する。
-foreach(/* ⑪の処理を書く */){
+foreach($_POST["books"] as $book){
 	/*
 	 * ⑫POSTの「stock」について⑩の変数の値を使用して値を取り出す。
 	 * 半角数字以外の文字が設定されていないかを「is_numeric」関数を使用して確認する。
 	 * 半角数字以外の文字が入っていた場合はif文の中に入る。
 	 */
-	if (/* ⑫の処理を書く */) {
+	if (!is_numric(($_POST["stock"][$book_count]))) {
 		//⑬SESSIONの「error」に「数値以外が入力されています」と設定する。
 		//⑭「include」を使用して「syukka.php」を呼び出す。
 		//⑮「exit」関数で処理を終了する。
+		$_SESSION['error'] = "数値以外が入力されています";
+		include("syukka.php");
+		exit();
 	}
 
 	//⑯「getByid」関数を呼び出し、変数に戻り値を入れる。その際引数に⑪の処理で取得した値と⑧のDBの接続情報を渡す。
 
 	//⑰ ⑯で取得した書籍の情報の「stock」と、⑩の変数を元にPOSTの「stock」から値を取り出して書籍情報の「stock」から値を引いた値を変数に保存する。
+	$book_data = getByid($book,$pdo);
+	$total_stock = $book_data["stock"] + $_POST["stock"][$book_count];
 
 	//⑱ ⑰の値が0未満か判定する。0未満の場合はif文の中に入る。
-	if(/* ⑱の処理を行う */){
+	if($total_stock < 0){
 		//⑲SESSIONの「error」に「出荷する個数が在庫数を超えています」と設定する。
 		//⑳「include」を使用して「syukka.php」を呼び出す。
 		//㉑「exit」関数で処理を終了する。
+		$_SESSION['error'] = "出荷する個数が在庫数を超えています";
+		include("syukka.php");
+		exit();
 	}
 	
 	//㉒ ⑩で宣言した変数をインクリメントで値を1増やす。
+	$book_count ++;
 }
 
 /*
  * ㉓POSTでこの画面のボタンの「add」に値が入ってるか確認する。
  * 値が入っている場合は中身に「ok」が設定されていることを確認する。
  */
-if(/* ㉓の処理を書く */){
+if(isset($_POST["add"]) && $_POST["add"] = "ok"){
 	//㉔書籍数をカウントするための変数を宣言し、値を0で初期化する。
 
+	$book_count = 0;
+	$bookPost = $_POST["books"];
+
 	//㉕POSTの「books」から値を取得し、変数に設定する。
-	foreach(/* ㉕の処理を書く */){
+	foreach($_POST["books"] as $book){
 		//㉖「getByid」関数を呼び出し、変数に戻り値を入れる。その際引数に㉕の処理で取得した値と⑧のDBの接続情報を渡す。
 		//㉗ ㉖で取得した書籍の情報の「stock」と、㉔の変数を元にPOSTの「stock」から値を取り出して書籍情報の「stock」から値を引いた値を変数に保存する。
 		//㉘「updateByid」関数を呼び出す。その際に引数に㉕の処理で取得した値と⑧のDBの接続情報と㉗で計算した値を渡す。
 		//㉙ ㉔で宣言した変数をインクリメントで値を1増やす。
+
+		$book_data = getByid($book,$pdo);
+		$total_stock = $book_data["stock"] + $_POST["stock"][$book_count];
+		updateByid($book,$pdo,$total_stock);
+
+		$book_count++;
 	}
 
 	//㉚SESSIONの「success」に「入荷が完了しました」と設定する。
 	//㉛「header」関数を使用して在庫一覧画面へ遷移する。
+	$_SESSION["success"] = "出荷が完了しました";
+	header("location:zaiko_ichiran.php");
+	
 }
 ?>
 <!DOCTYPE html>
@@ -113,8 +168,9 @@ if(/* ㉓の処理を書く */){
 					//㉜書籍数をカウントするための変数を宣言し、値を0で初期化する。
 
 					//㉝POSTの「books」から値を取得し、変数に設定する。
-					foreach(/* ㉝の処理を書く */){
+					foreach($_POST['books'] as $book){
 						//㉞「getByid」関数を呼び出し、変数に戻り値を入れる。その際引数に㉜の処理で取得した値と⑧のDBの接続情報を渡す。
+						$book_data = getByid($book,$pdo)
 					?>
 					<tr>
 						<td><?php echo	/* ㉟ ㉞で取得した書籍情報からtitleを表示する。 */;?></td>
